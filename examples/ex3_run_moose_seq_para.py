1e8d82904fcbc541c0fbd944bcfd936123107e88
ScepticalRabbit <thescepticalrabbit@gmail.com>
2023-12-17 19:15:49 +0000

    First draft of gmsh running with moose in example 4

--- examples/ex3_run_moose_seq_para.py
+++ examples/ex3_run_moose_seq_para.py
@@ -12,7 +12,6 @@ from mooseherder import MooseRunner
 from mooseherder import InputModifier
 
 if __name__ == '__main__':
-    line_str = ''
     print('------------------------------------------')
     print('EXAMPLE 3: Herd Setup')
     print('------------------------------------------')
@@ -33,7 +32,7 @@ if __name__ == '__main__':
     herd.clear_dirs()
     herd.create_dirs(one_dir=False)
 
-    # Create variables to sweep in a list of dictionaries
+    # Create variables to sweep in a list of dictionaries, 8 combinations possible.
     n_elem_y = [25,50]
     e_mod = [1e9,2e9]
     p_rat = [0.3,0.35]
@@ -44,15 +43,16 @@ if __name__ == '__main__':
                 moose_vars.append({'n_elem_y':nn,'e_modulus':ee,'p_ratio':pp})
 
 
-    # Set the parallelisation options
+    # Set the parallelisation options, we have 8 combinations of variables and
+    # 4 MOOSE intances running, so 2 runs will be saved in each working directory
     herd.para_opts(n_moose=4,tasks_per_moose=1,threads_per_moose=2,redirect_out=True)
-    
     
     print()
     print('------------------------------------------')
     print('EXAMPLE 3a: Run MOOSE once')
     print('------------------------------------------')
     
+    # Single run saved in moose-workdir-1
     herd.run_once(0,moose_vars[0])
 
     print('Run time (once) = '+'{:.3f}'.format(herd.get_iter_time())+' seconds')
@@ -62,6 +62,7 @@ if __name__ == '__main__':
     print('EXAMPLE 3b: Run MOOSE sequentially')
     print('------------------------------------------')
 
+    # Run all variable combinations (8) sequentially in moose-workdir-1
     herd.run_sequential(moose_vars)
 
     print('Run time (sequential) = '+'{:.3f}'.format(herd.get_sweep_time())+' seconds')
@@ -71,6 +72,8 @@ if __name__ == '__main__':
     print('EXAMPLE 3c: Run MOOSE in parallel')
     print('------------------------------------------')
 
+    # Run all variable combinations across 4 MOOSE instances with two runs saved in
+    # each moose-workdir
     herd.run_para(moose_vars)
 
     print('Run time (parallel) = '+'{:.3f}'.format(herd.get_sweep_time())+' seconds')
--- src/mooseherder/__init__.py
+++ src/mooseherder/__init__.py
@@ -7,5 +7,6 @@ from mooseherder.mooseherd import *
 from mooseherder.inputmodifier import *
 from mooseherder.mooserunner import *
 from mooseherder.gmshrunner import *
+from mooseherder.outputreader import *
 
-__all__ = ["mooseherd" "inputmodifier" "mooserunner" "gmshrunner"]
+__all__ = ["mooseherd" "inputmodifier" "mooserunner" "gmshrunner" "outputreader"]
