22d7836e833a186fe74b3b29030e542c8f84b954
ScepticalRabbit <thescepticalrabbit@gmail.com>
2024-02-02 08:15:15 +0000

    Linted run all examples script.

--- examples/run_all.py
+++ examples/run_all.py
@@ -7,15 +7,16 @@ Author: Lloyd Fletcher
 '''
 import os
 import subprocess
+from pathlib import Path
 from pprint import pprint
 
-example_dir = 'examples/'
-all_files = os.listdir(example_dir)
+EXAMPLE_DIR = Path('examples/')
+all_files = os.listdir(EXAMPLE_DIR)
 
-example_files = list()
+example_files = list([])
 for ff in all_files:
     if ('ex' in ff) and ('.py' in ff):
-        example_files.append(example_dir + ff)
+        example_files.append(EXAMPLE_DIR / ff)
 
 example_files.sort()
 print('Running all examples files listed below:')
@@ -23,11 +24,7 @@ pprint(example_files)
 print()
 
 for ee in example_files:
-    run_str = 'python '+ ee
+    run_str = 'python '+ str(ee)
     print(run_str)
     subprocess.run(run_str, shell=True)
-
-
-
-
 
