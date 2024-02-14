9fdd7e7fec462251341bb76653639ce5465c6867
ScepticalRabbit <thescepticalrabbit@gmail.com>
2024-02-14 17:10:50 +0000

    Finished doc strings for gmshrunner

--- src/mooseherder/gmshrunner.py
+++ src/mooseherder/gmshrunner.py
@@ -11,10 +11,11 @@ from mooseherder.simrunner import SimRunner
 
 class GmshRunner(SimRunner):
     """Used to call gmsh to create a mesh file to be used to run a finite
-    element simulation.
+    element simulation. Implements the SimRunner abstract interface so that it
+    can be used by the herd.
     """
     def __init__(self, gmsh_app: Path | None = None):
-        """Create a gmsh runner with path to gmsh app.
+        """Create a gmsh runner with path to the gmsh app.
 
         Args:
             gmsh_app (Path, optional): full path to the gmsh app. Defaults to None.
@@ -43,15 +44,15 @@ class GmshRunner(SimRunner):
 
 
     def get_input_file(self) -> Path | None:
-        """get_input_path _summary_
+        """get_input_path: the path to the input file to run gmsh with.
 
         Returns:
-            Path | None: _description_
+            Path | None: path to the gmsh *.geo file.
         """
         return self._input_path
 
 
-    def set_input_file(self, input_file: Path) -> None:
+    def set_input_file(self, input_path: Path) -> None:
         """Sets the input geo file for gmsh.
 
         Args:
@@ -61,13 +62,14 @@ class GmshRunner(SimRunner):
             FileNotFoundError: Not a .geo file
             FileNotFoundError: Geo file does not exist
         """
-        if input_file.suffix != '.geo':
+        if input_path.suffix != '.geo':
             raise FileNotFoundError('Incorrect file type. Must be *.geo.')
 
-        if not input_file.exists():
+        if not input_path.exists():
             raise FileNotFoundError('Specified gmsh geo file does not exist.')
 
-        self._input_path = input_file
+        self._input_path = input_path
+
 
     def run(self, input_file: Path | None = None) -> None:
         """Run the geo file to create the mesh.
@@ -95,6 +97,14 @@ class GmshRunner(SimRunner):
         self._run_str = f'{self._gmsh_app} {self._input_path}'
         os.system(self._run_str)
 
+
     def get_output_path(self) -> Path | None:
+        """get_output_path: default return None for gmsh as there is no output
+        to be read after the simulation has run. This information is stored in
+        the exodus.
+
+        Returns:
+            Path | None: Default returns None.
+        """
         return None
 
