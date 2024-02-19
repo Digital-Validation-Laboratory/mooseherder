a0acce4eb64a610b573cdcb73f12925b574a0583
ScepticalRabbit <thescepticalrabbit@gmail.com>
2024-02-19 19:18:40 +0000

    Merged RS fix

--- src/mooseherder/sweepreader.py
+++ src/mooseherder/sweepreader.py
@@ -77,15 +77,18 @@ class SweepReader:
         """
         output_paths = self._find_files_by_str(self._dir_manager.get_output_key_tag(),
                                                self._dir_manager.get_run_dir(0))
+        #print(output_paths)
 
         if len(output_paths) == 0:
             raise FileNotFoundError("No output key json files found.")
 
         output_files = list([])
-        for ii,_ in enumerate(output_paths):
-            output_files = output_files + self.read_output_key(ii+1)
+        for ii,output_path in enumerate(output_paths):
+            sweep_iter = output_path.name.split('.')[0].split('-')[-1]
+            output_files = output_files + self.read_output_key(int(sweep_iter))
 
         self._output_files = output_files
+        #print(output_files)
         return self._output_files
 
 
@@ -222,7 +225,7 @@ class SweepReader:
         sweep_results = list([])
         for ll in self._output_files:
             for ff in ll:
-                if ff is not None:
+                if ff.exists():
                     sweep_results.append(self.read_results_once(ff,read_config))
 
         return sweep_results
